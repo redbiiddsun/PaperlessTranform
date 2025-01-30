d6da9bc79fbe468aafee3be78c77318966bf449d
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2025-01-30 16:52:28 +0700

    feat: add a LlamaWrapper class for parent class

---
+++ development/DataTypeAnalyzer/llama_wraper.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod
+from typing import List, Dict
+from ollama import chat, ChatResponse
+
+class LlamaWrapper():
+    """
+    A wrapper class for interacting with LLaMA models.
+    """
+    
+    def __init__(self):
+        self.model_name = 'llama3.2'
+
+    def set_model(self, model_name: str) -> None:
+        self.model_name = model_name
+
+    def chat(self, messages: List[Dict]) -> ChatResponse:
+        try:
+            response: ChatResponse = chat(
+                model=self.model_name,
+                messages=messages
+            )
+            return response
+        except Exception as e:
+            raise Exception(f"Error analyzing fields: {str(e)}")

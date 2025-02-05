890ff74f335d0c5fa9fcb3b7dae6c704cae5db41
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2025-02-05 23:54:40 +0700

    feat: implement text detection functionality using pytesseract and easyocr

---
+++ development/detect_form/text_detection.py
@@ -0,0 +1,26 @@
+import pytesseract
+import cv2
+import easyocr
+import matplotlib.pyplot as plt
+
+
+from utils import Utils
+class TextDetection:
+
+    def __init__(self):
+        pass
+
+    def detect_text(self, image):
+        custom_config = r'-l tha --oem 3 --psm 6 --dpi 2400'
+        data = pytesseract.pytesseract.image_to_string(image, config=custom_config)
+        print(data)
+
+    def easy_ocr(self, image):
+        reader = easyocr.Reader(['en', 'th'], gpu=True)
+        result = reader.readtext(image, detail = 0, paragraph=False)
+        print(result)
+
+
+tmp = TextDetection().detect_text(Utils().preprocess_image('./image/Medium/2.png'))
+
+# tmp = TextDetection().keras_ocr('./image/Quality/1.png')
